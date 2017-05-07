3235cc66c1a9009aa76d793069a6586d3a3b20b2
Rafael Gonçalves <rafagonc77@yahoo.com.br>
2017-05-07 18:23:42 -0300

    init

---
+++ QuemEuBeijeiOntem/QEBOContactListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QEBOContactListViewController.swift
+//  QuemEuBeijeiOntem
+//
+//  Created by Rafael Gonzalves on 07/05/17.
+//  Copyright © 2017 Rafael Gonzalves. All rights reserved.
+//
+
+import UIKit
+
+class QEBOContactListViewController: UIViewController {
+    
+    //MARK: Constructor
+    init() {
+        super.init(nibName: "QEBOContactListViewController", bundle: nil)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+
+    //MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+}
