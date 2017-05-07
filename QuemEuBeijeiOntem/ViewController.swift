878d40f88fe831cd4a349e8130838f0d9322d7ea
Rafael Gonçalves <rafagonc77@yahoo.com.br>
2017-05-07 18:15:34 -0300

    Initial Commit

---
+++ QuemEuBeijeiOntem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuemEuBeijeiOntem
+//
+//  Created by Rafael Gonzalves on 07/05/17.
+//  Copyright © 2017 Rafael Gonzalves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
